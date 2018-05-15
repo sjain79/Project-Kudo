24c6ab277a74bb894130d7eb497809999ee9c19e
Siddharth <sjain@student.fullsail.edu>
2018-05-15 17:41:58 -0400

    Made the GameScene Made the Jump script for the player to automatically jump after colliding with the platform The player is moving with the movement script using the keyboard keys Added the all sprites

---
+++ Project Kudo/Assets/JumpScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpScript : MonoBehaviour
+{
+
+    [SerializeField]
+    float jumpForce;
+
+    Rigidbody2D playerRb;
+
+    private void Awake()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.relativeVelocity.y >= 0f)
+        {
+
+            if (collision.gameObject.CompareTag("Platform"))
+            {
+                Vector2 jumpVector = new Vector2(playerRb.velocity.x, jumpForce);
+                playerRb.velocity = jumpVector;
+            }
+        }
+    }
+}
---
+++ Project Kudo/Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    float horizontalMovementMultiplier;
+
+    Vector2 movement;
+
+    Rigidbody2D playerRb;
+
+    private void Awake()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        movement = new Vector2(Input.GetAxis("Horizontal") * horizontalMovementMultiplier, playerRb.velocity.y);
+    }
+
+    private void FixedUpdate()
+    {
+        playerRb.velocity = movement;
+    }
+}
