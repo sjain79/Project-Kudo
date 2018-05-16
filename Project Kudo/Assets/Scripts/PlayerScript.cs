3d8388cecfbaf4ff8d9fa33558b3cb7de711ce07
Siddharth <sjain@student.fullsail.edu>
2018-05-16 09:27:21 -0400

    Enemy implemented

---
+++ Project Kudo/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    Rigidbody2D playerRb;
+    JumpScript myJumpScript;
+
+    [SerializeField]
+    float diedUpForce;
+
+    private void Awake()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+        myJumpScript = GetComponent<JumpScript>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            myJumpScript.enabled = false;
+            playerRb.velocity = new Vector2(playerRb.velocity.x, diedUpForce);
+            playerRb.GetComponent<Collider2D>().enabled = false;
+            Debug.Log("Player Died");
+        }
+    }
+}
