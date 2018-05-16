1fd7e869e256a9153710793cbc62e8c1a26af525
Siddharth Jain <sjain@student.fullsail.edu>
2018-05-16 03:06:41 -0400

    Spring Jump platform implemented

---
+++ Project Kudo/Assets/Scripts/SpringJumpPlatform.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpringJumpPlatform : MonoBehaviour
+{
+    [SerializeField]
+    float jumpForce;
+
+    bool wasUsed = false;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.relativeVelocity.y <= 0f)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                if (!wasUsed)
+                {
+                    Rigidbody2D playerRb = collision.gameObject.GetComponent<Rigidbody2D>();
+                    Vector2 jumpVector = new Vector2(playerRb.velocity.x, jumpForce);
+                    playerRb.velocity = jumpVector;
+
+                    Animator springAnim = GetComponent<Animator>();
+                    springAnim.SetTrigger("Spring Jump");
+
+                    wasUsed = true;
+                }
+            }
+        }
+    }
+
+}
