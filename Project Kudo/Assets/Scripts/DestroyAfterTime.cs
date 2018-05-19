faa0467cb5b3209172e93c89f550f33edf6cd204
Unknown <sjain@student.fullsail.edu>
2018-05-19 14:40:55 -0400

    Script to destroy things after time

---
+++ Project Kudo/Assets/Scripts/DestroyAfterTime.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterTime : MonoBehaviour {
+
+    [SerializeField]
+    float time;
+
+    private void Start()
+    {
+        Destroy(gameObject, time);
+    }
+}
