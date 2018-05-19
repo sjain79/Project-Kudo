f9050ded1afd8aefaf559bcbc814df5e06b52f00
Unknown <sjain@student.fullsail.edu>
2018-05-19 14:40:38 -0400

    Additional gold coin assets

---
+++ Project Kudo/Assets/Scripts/GoldCoinScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldCoinScript : MonoBehaviour {
+
+    [SerializeField]
+    GameObject goldCoinEffectPrefab;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<PlayerScript>().coins++;
+            Instantiate(goldCoinEffectPrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
