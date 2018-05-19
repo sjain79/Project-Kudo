d10deaaa06d4ebbd52b70f70b1d9c7590c66de34
Unknown <sjain@student.fullsail.edu>
2018-05-19 14:40:17 -0400

    Wing Man Enemy created

---
+++ Project Kudo/Assets/Scripts/WingManMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WingManMovement : MonoBehaviour {
+
+    [SerializeField]
+    float speed,strength;
+
+    private void Update()
+    {
+        Vector2 newPosition = transform.position;
+        newPosition.x = Mathf.Sin(Time.time * speed) * strength;
+        transform.position = newPosition;
+    }
+}
