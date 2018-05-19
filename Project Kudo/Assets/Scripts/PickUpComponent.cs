778873e3b25b017f9238605f936047740b3fc79c
Unknown <sjain@student.fullsail.edu>
2018-05-19 14:38:59 -0400

    A script created to identify the type of pick-up

---
+++ Project Kudo/Assets/Scripts/PickUpComponent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PickUpType { GoldCoin,Bubble};
+
+public class PickUpComponent : MonoBehaviour
+{
+    [SerializeField]
+    public PickUpType pickUpType;
+}
